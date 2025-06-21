a8a677b23c64b8cfe517856bb2675136cf3b3a1d
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-06-21 23:04:55 +0530

    added verify route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,19 @@
 const { Router } = require("express");
 const { addUserInDatabase, provideJWT } = require("../controllers/userController.js");
 const { verifyUserType, checkUserExistInDB } = require("../middlewares/user.js");
+const { verifyJWT } = require("../middlewares/transaction.js")
 const router = Router();
 
 router.post("/signup", verifyUserType, addUserInDatabase);
 
 // sending jwt to user in this route-handler
 router.post("/signin", verifyUserType, checkUserExistInDB, provideJWT);
+
+router.post("/verify", verifyJWT, function(req, res, next) {
+    res.status(200).json({
+        msg: "verification successfull"
+    })
+});
 
 // directly exporting router
 module.exports = router;
